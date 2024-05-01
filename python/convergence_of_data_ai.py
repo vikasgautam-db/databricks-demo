735b3fc71f310c9986be78e1109bc857e3b20c2c
vikasgautam-db <vikasgautam-db>
2024-05-01 15:41:55 +0000

    day end check-in

--- python/convergence_of_data_ai.py
+++ python/convergence_of_data_ai.py
@@ -1,48 +1,69 @@
 # Databricks notebook source
 # MAGIC %md-sandbox
-# MAGIC <h1 style="text-align: center;">Convergance of Data and Artificial Intelligence</h1>
+# MAGIC <h1 style="text-align: left;">Convergance of Data and Artificial Intelligence</h1>
 # MAGIC
-# MAGIC <img src="https://raw.githubusercontent.com/vikasgautam-db/databricks-demo/main/resources/di-day-mumbai/converge_data_ai_padded.png" style="float: left; margin-right: 10px" width="600px" />
+# MAGIC <img src="https://raw.githubusercontent.com/vikasgautam-db/databricks-demo/main/resources/di-day-mumbai/converge_data_ai_updated.png" style="float: center; margin-left: 100px; margin-right: 10px" width="900px" />
 # MAGIC
-# MAGIC <br/>
+# MAGIC <div style="padding-left: 30px; margin-right: 200px">
 # MAGIC
-# MAGIC <br/>
-# MAGIC <div style="padding-left: 610px">
-# MAGIC
-# MAGIC ### Strong Data Foundation
-# MAGIC Data lakehouse provides Good Quality reliable data and acts as a single source of truth for the entire enterprise.
+# MAGIC #### Strong Data Foundation
+# MAGIC Data lakehouse provides good quality reliable data and acts as a single source of truth for the entire enterprise. It brings together data of differnt formats, provides intelligent workload management and unified governance capabilities. 
 # MAGIC More data can be leveraged, more often to drive more value for organisations
 # MAGIC
-# MAGIC ### Advanced AI Capabilities
+# MAGIC #### Advanced AI Capabilities
 # MAGIC Databricks provides a unified AI infused intelligent platform that largely simplifies the development and deployment of AI applications. One can build more reliable production grade AI applications much faster while maintaining complete data privacy, explainability and competitive advantage.
 # MAGIC
+# MAGIC #### Data Intelligence
+# MAGIC The amalgamation of these two creates a fly wheel effect - the more data is fed into AI models, the more they learn and improve their predictive power. This has a transformative impact on business intelligence and decision-making thus resulting in more data. Organizations across industries are harnessing this convergence to make data-driven decisions, automate complex tasks, and build innovative products and services.
 # MAGIC
-# MAGIC </br>
 # MAGIC
 # MAGIC
 
 # COMMAND ----------
 
 # MAGIC %md-sandbox
-# MAGIC <h1 style="text-align: center;">Convergance of Data and Artificial Intelligence</h1>
+# MAGIC <h1 style="text-align: left; color: black">You need more than just good models !!</h1>
 # MAGIC
-# MAGIC <img src="https://raw.githubusercontent.com/vikasgautam-db/databricks-demo/main/resources/di-day-mumbai/converge_data_ai_updated.png" style="float: left-center; margin-right: 10px" width="1000px" />
-# MAGIC
-# MAGIC <br/>
-# MAGIC
-# MAGIC <br/>
-# MAGIC <div style="padding-left: 10px">
-# MAGIC
-# MAGIC ### Strong Data Foundation
-# MAGIC Data lakehouse provides Good Quality reliable data and acts as a single source of truth for the entire enterprise.
-# MAGIC More data can be leveraged, more often to drive more value for organisations
-# MAGIC
-# MAGIC ### Advanced AI Capabilities
-# MAGIC Databricks provides a unified AI infused intelligent platform that largely simplifies the development and deployment of AI applications. One can build more reliable production grade AI applications much faster while maintaining complete data privacy, explainability and competitive advantage.
+# MAGIC <img src="https://raw.githubusercontent.com/vikasgautam-db/databricks-demo/main/resources/di-day-mumbai/challenges_overview_2.png" style="float: center; margin-left: 100px; margin-right: 10px" width="900px" />
 # MAGIC
 # MAGIC
-# MAGIC </br>
+
+# COMMAND ----------
+
+# MAGIC %md-sandbox
 # MAGIC
+# MAGIC <img src="https://raw.githubusercontent.com/vikasgautam-db/databricks-demo/main/resources/di-day-mumbai/dip_mosaic_overview_wb.png" style="float: center; margin-right: 10px" width="900px" />
+# MAGIC
+# MAGIC
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md-sandbox
+# MAGIC
+# MAGIC <img src="https://raw.githubusercontent.com/vikasgautam-db/databricks-demo/main/resources/di-day-mumbai/GenAI_architecture_patterns.png" style="float: center; margin-right: 10px" width="900px" />
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md ### Demo Prompting Techniques
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md ### Limitations of Prompting Techniques
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md ### Demo RAG Example
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md ### Demo Fine Tuning Example
 # MAGIC
 
 # COMMAND ----------
