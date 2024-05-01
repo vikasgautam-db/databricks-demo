13c3fc909cd7c58bba1d5583858a34fffd5d0135
vikasgautam-db <vikasgautam-db>
2024-05-01 06:52:20 +0000

    more resources

---
+++ python/convergence_of_data_ai.py
@@ -0,0 +1,27 @@
+# Databricks notebook source
+# MAGIC %md-sandbox
+# MAGIC <h1 style="text-align: center;">Convergance of Data and Artificial Intelligence</h1>
+# MAGIC
+# MAGIC <img src="https://raw.githubusercontent.com/vikasgautam-db/databricks-demo/main/resources/di-day-mumbai/converge_data_ai_updated.png" style="float: left; margin-right: 30px" width="800px" />
+# MAGIC
+# MAGIC <br/>
+# MAGIC
+# MAGIC ## Accessible to everyone
+# MAGIC The ability to interact with the data in natural language, provides ease of use and simplifies data for everyone
+# MAGIC More data can be leveraged, more often to drive more value for organisations
+# MAGIC
+# MAGIC ## Simple to Operate
+# MAGIC Intelligence infused throughout the platform, means AI can be applied to deliver automations and optimisations in a way that is specific to an organisation
+# MAGIC
+# MAGIC ## Build your own Private AI
+# MAGIC You’re not giving your data or IP away to anyway. 
+# MAGIC You own the data, and IP of your models, maintaining your privacy and competitive advantage.
+# MAGIC
+# MAGIC
+# MAGIC
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+
