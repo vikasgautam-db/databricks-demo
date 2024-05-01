be07e589b9e9bdcae2cf28b9f72768487e29478a
vikasgautam-db <vikasgautam-db>
2024-05-01 07:16:35 +0000

    improved image

--- python/convergence_of_data_ai.py
+++ python/convergence_of_data_ai.py
@@ -2,23 +2,46 @@
 # MAGIC %md-sandbox
 # MAGIC <h1 style="text-align: center;">Convergance of Data and Artificial Intelligence</h1>
 # MAGIC
-# MAGIC <img src="https://raw.githubusercontent.com/vikasgautam-db/databricks-demo/main/resources/di-day-mumbai/converge_data_ai_updated.png" style="float: left; margin-right: 30px" width="800px" />
+# MAGIC <img src="https://raw.githubusercontent.com/vikasgautam-db/databricks-demo/main/resources/di-day-mumbai/converge_data_ai_padded.png" style="float: left; margin-right: 10px" width="600px" />
 # MAGIC
 # MAGIC <br/>
 # MAGIC
-# MAGIC ## Accessible to everyone
-# MAGIC The ability to interact with the data in natural language, provides ease of use and simplifies data for everyone
+# MAGIC <br/>
+# MAGIC <div style="padding-left: 610px">
+# MAGIC
+# MAGIC ### Strong Data Foundation
+# MAGIC Data lakehouse provides Good Quality reliable data and acts as a single source of truth for the entire enterprise.
 # MAGIC More data can be leveraged, more often to drive more value for organisations
 # MAGIC
-# MAGIC ## Simple to Operate
-# MAGIC Intelligence infused throughout the platform, means AI can be applied to deliver automations and optimisations in a way that is specific to an organisation
-# MAGIC
-# MAGIC ## Build your own Private AI
-# MAGIC You’re not giving your data or IP away to anyway. 
-# MAGIC You own the data, and IP of your models, maintaining your privacy and competitive advantage.
+# MAGIC ### Advanced AI Capabilities
+# MAGIC Databricks provides a unified AI infused intelligent platform that largely simplifies the development and deployment of AI applications. One can build more reliable production grade AI applications much faster while maintaining complete data privacy, explainability and competitive advantage.
 # MAGIC
 # MAGIC
+# MAGIC </br>
 # MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md-sandbox
+# MAGIC <h1 style="text-align: center;">Convergance of Data and Artificial Intelligence</h1>
+# MAGIC
+# MAGIC <img src="https://raw.githubusercontent.com/vikasgautam-db/databricks-demo/main/resources/di-day-mumbai/converge_data_ai_updated.png" style="float: left-center; margin-right: 10px" width="1000px" />
+# MAGIC
+# MAGIC <br/>
+# MAGIC
+# MAGIC <br/>
+# MAGIC <div style="padding-left: 10px">
+# MAGIC
+# MAGIC ### Strong Data Foundation
+# MAGIC Data lakehouse provides Good Quality reliable data and acts as a single source of truth for the entire enterprise.
+# MAGIC More data can be leveraged, more often to drive more value for organisations
+# MAGIC
+# MAGIC ### Advanced AI Capabilities
+# MAGIC Databricks provides a unified AI infused intelligent platform that largely simplifies the development and deployment of AI applications. One can build more reliable production grade AI applications much faster while maintaining complete data privacy, explainability and competitive advantage.
+# MAGIC
+# MAGIC
+# MAGIC </br>
 # MAGIC
 # MAGIC
 
